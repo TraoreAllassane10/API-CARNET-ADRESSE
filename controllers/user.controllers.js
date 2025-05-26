8b92a9b67987e45817a23257c699e48471bdd6bd
TraoreAllassane10 <traoreallassane2255@gmail.com>
2025-05-26 11:12:53 +0000

    Factorise user controller avec le service pattern

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -1,57 +1,22 @@
-const User = require("../models/User");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const PrivateKey = require("../auth");
+const userServices = require("../services/user.services")
 
 const register = async (req, res) => {
+
   let { name, email, password } = req.body;
 
-  const verifiedEmail = await User.findOne({ email });
+  const response = await userServices.create({name, email, password});
 
-  if (verifiedEmail) {
-    res.json({ message: "Cet utilisateur existe déjà" });
-    return;
-  }
+  res.json(response)
 
-  password = bcrypt.hashSync(password, 10);
-
-  try {
-    const user = await User.create({ name, email, password });
-
-    if (user) {
-      res.json({ message: "Utilisateur crée", data: user });
-    } else {
-      res.json({ message: "Erreur lors de la creation de l'utilisateur" });
-    }
-  } catch (error) {
-    res.json({ message: "Erreur survenue du coté du serveur" });
-  }
 };
 
 const login = async (req, res) => {
   let { email, password } = req.body;
 
-  const user = await User.findOne({ email });
+  const {code, response} = await userServices.login({email, password});
 
-  if (!user) {
-    res.status(404).json({ message: "Vous n'avez de compte" });
-    return;
-  }
+  res.status(code).json(response);
 
-  const validated = bcrypt.compareSync(password, user.password);
-
-  if (validated) {
-    const token = jwt.sign(
-      { id: user._id, name: user.name, email: user.email, admin: user.admin },
-      PrivateKey,
-      { expiresIn: "24h" }
-    );
-    res
-      .status(200)
-      .json({ message: "Utilisateur connecté", data: user, token });
-  } else {
-    res.status(401).json({ message: "Mot de passe incorrect" });
-  }
 };
 
 module.exports = { register, login };
